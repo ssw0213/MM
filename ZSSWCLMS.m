04c5d1668669090076be472f946af65bb12f75d7
ssw0213 <swatson@mmwv.co>
2015-07-31 17:54:57 -0400

    Begin v1.0

--- ZSSWCLMS.m
+++ ZSSWCLMS.m
@@ -1,42 +1,58 @@
-ZSSWCLMS   ; Routine to remove REV*G5 entries from KEY BENEFIT ADMINISTRATORS records
+SWCLM4   ; Routine to remove REV*G5 and REV*G2 entries from EDI files
                 ; SSW 3150527
-                ; FN is path to input file. Output goes to "FN.cleaned"
-        N X,Z,EOFT,TMPZ,DT,D,LN,B,S,E,I,J,IN,Segs,KEY,G5,KF,FNO,LIN,LS,LO,CRLF,CLOG   ; New variables.
-	S TMPZ="/tmp/zsytemp",DT=$ZDATE($H,"YYYYMMDD"),CLOG="claims."_DT_"/out/log."_DT,$P(LN,"-",40)=""
-	ZSY "mkdir claims."_DT_" ; cd claims."_DT_" ; mkdir in out"
-	ZSY "find . -maxdepth 1 -type f > "_TMPZ
-	O TMPZ O CLOG S EOFT=0 U TMPZ:exception="D EOF"
-	F J=1:1 Q:EOFT  U TMPZ R FN S $E(FN,1,2)="",Z(J)=FN D  
-	.S FNO="claims."_DT_"/out/"_FN_".cleaned" 
-	.U CLOG W !,J,?4,FN," > ",FNO U 0 W !,J,?4,FN," > ",FNO D CLEAN 
-	.ZSY "mv "_FN_" claims."_DT_"/in"
-	U 0 W !,"End",!
-	Q	
-EOF	I '$zeof zmessage +$zstatus
-	C TMPZ S EOFT=1
-	Q
-CLEAN ; Clean file named FN
-        O FN:(readonly:fixed:recordsize=32767)  ; set up for binary read
-	U FN S IN="" F I=1:1 R X Q:X=""  S IN=IN_X  ; Read input file into local variable IN
-        U CLOG C FN S D="NM1*85",KEY="KEY BENEFIT",G5="REF*G5*"  ; Close file, Set variables
-	S LIN=$L(IN),IN=$TR(IN,$C(10)_$C(13)),CRLF=LIN-$L(IN)
-        S X(1)=$P(IN,D) F I=2:1 Q:$P(IN,D,I)=""  S X(I)=D_$P(IN,D,I)  ; Chunk into segments in X(I) array
-        S Segs=I-1  ; 
-	W !,"Found ",Segs," segments using delimiter ",D  ; Print header
-	W !,"Looking for segments containing ",KEY," with string ",G5  ; Print header 1
-	W !!,"Segment",?10,"String",?40,"Location",?50,"Length"  ; Print header 2
-	S KF=0,LS=0 F I=1:1:Segs I X(I)[KEY S KF=1 D  ; loop through segments looking for KEY then excise G5
-	.I X(I)[G5 D  
-	..S B=$F(X(I),G5)-$L(G5),E=$F(X(I),$C(126),B)-1,S=$E(X(I),B,E),LS=LS+$L(S),$E(X(I),B,E)="";  $C(126)="~"
-	..W !,I,?10,S,?40,B,?50,$L(S)  ; write what you did
-	.E  W !,I,?10,G5," not found"  ; G5 entry not found
-	.Q
-	W:'KF !,KEY," not found"  ; KEY entry not found
-	O FNO:(newversion:stream:nowrap:chset="M")  ; set up for binary write
-	U FNO S LO=0  ; set up output file
-	F I=1:1:Segs W X(I) S LO=LO+$L(X(I))    ; Write revised segments to output file
-        C FNO U CLOG W !,"Stripped ",CRLF," CR and LF characters, excised ",LS," string characters."
-	W !,"Input file length = ",LIN,", output file length = ",LO,!,LN,!
-	ZSY "cp "_FNO_" /home/opus/share" 
+           ;Place all files to be cleaned in directory ~/cleaning, 
+           ; FN is path to input file. Output goes to "FN.clean"
+INIT    N X,Z,EOFT,P,PWD,TMPZ,DT,D,DASHLINE,B,S,E,I,J,K,NREV,NF,IN,SEGS,KEY,G2,G5,MC0,SF,FNO,LIN,LS,LO,CRLF,CDIR,CLOG   ; New variables.
+        S NREV=4  ; Revision number of this routine
+        S G2="REF*G2*",G5="REF*G5*",D=$C(126)  ; $C(126)="~"
+        ; set up a temp file for filenames, a date string, a logfile path, and  a line string
+	S DT=$ZDATE($H,"YYYYMMDD")
+	S FP="FilePipe"
+	O FP:(command="pwd":readonly)::"PIPE" U FP R PWD C FP
+	S CDIR=PWD_"/claims."_DT,CLOG=CDIR_"/out/log"_NREV_"."_DT,$P(DASHLINE,"-",40)=""
+        ; Use linux system calls to make a dated claims directory with in and out subdirectories
+        ZSY "mkdir -p "_CDIR_"/in "_CDIR_"/out"  
+GETF   	O FP:(command="find /home/opus/share/ -maxdepth 1 -type f":readonly)::"PIPE"
+	F I=1:1 U FP R X Q:$ZEOF  S FN(I)=$TR(X," ","_") S:X["clean" I=I-1 ; Read filenames, no spaces, skip if clean already	
+	C FP S NF=I-1 O CLOG ; Close FilePipe, Save Number of Files, Open Log
+	F I=1:1:NF D  ; Go through and clean each file
+	.S FN=$P(FN(I),"/",$L(FN(I),"/")) ; Save FileName without path
+	.S FNO=CDIR_"/out/"_FN_".clean"_NREV  ; Name of output file
+        .S SEGO=PWD_"/segfiles/"_$P(FN(I),"/",$L(FN(I),"/"))  ; Name of a segmented file in segfile directory
+        .U CLOG W !,I,?4,FN(I)," > ",!,?4,FNO U 0 W !,I,?4,FN(I) D CLEAN   ; Write the filename to Log and 0, then clean it
+	.ZSY "cp "_FN(I)_" "_CDIR_"/in"  ; copy the input file to "in" 
+        C CLOG U 0 W !,"Processed ",NF," files",!  ; print to screen when done
+        Q       ; all done
+CLEAN ; Clean file named FN(I)
+        S MC0=$S($TR(FN(I),"MEDICAR","medicar")["medicar":"99212*0*",1:"notMedicare") ; MC0 search variable
+        ; Translate upper case "MEDICARE" to lower case and check to see if filename contains either one.
+        ; If so,  set up to search for 99212*0*, otherwise "notMedicare" which will not be found
+        O FN(I):(readonly:fixed:recordsize=32767)  ; set up for binary read
+        U FN(I) S IN="" F J=1:1 R X Q:X=""  S IN=IN_X  ; Read input file into local variable IN
+        C FN(I)  ; Close input file
+        S LIN=$L(IN),IN=$TR(IN,$C(10)_$C(13)),CRLF=LIN-$L(IN)  ; strip linefeeds and carriage returns
+	I IN["NM1*41*2*CENTRAL" S LS=0 U CLOG W !,"CENTRAL LABS File: Strip CRLF only" G FINISH
+        S X(0)="" O SEGO U SEGO ; open segmented file, get ready to write initial ""
+        F J=1:1 W X(J-1),! Q:$P(IN,D,J)?." "  S X(J)=$P(IN,D,J)_D ; Chunk into segments in X(J) array
+        S SEGS=J-1 ;  Save number of segments. Note- omit last Seg if it is all spaces
+        C SEGO O CLOG U CLOG
+        W !,"Found ",SEGS," segments using delimiter ",D  ; Print header 1
+        W !,"Looking for search strings ",G2,", ",G5,", 99212*0*"  ; Print header 2
+        W !!,"Seg",?5,"String",?75,"Len" ; Print header 3
+        S (LS,SF)=0 F J=1:1:SEGS D  ; loop through segments looking for strings to excise
+        .I X(J)[G2 S S=X(J),X(J)="" D  ; find and excise G2
+        ..I X(J+2)[G2 S S=X(J-1)_S_X(J+1)_X(J+2),(X(J-1),X(J+1),X(J+2))="",J=J+2 Q
+        ..; Two consecutive G2 segments will also have two context segments. Excise all four.
+        .I X(J)[G5 S S=X(J),X(J)="" Q; same for G5
+        .I X(J)[MC0 S S=X(J-1)_X(J)_X(J+1)_X(J+2),(X(J-1),X(J),X(J+1),X(J+2))="",J=J+2 Q
+        .; find and excise MC0 and its context segments, one preceding and two following
+        .I X(J)="" S SF=1,LS=LS+$L(S) W !,J-(X(J-1)=""),?5,S,?75,$L(S); Write what you did
+        W:SF=0 !," no search strings found"  ; not found
+        S IN="" F K=1:1:SEGS S IN=IN_X(K)    ; Write revised segments to file, save in IN
+FINISH  O FNO:(newversion:stream:nowrap:chset="M") ; change to no-line EDI file
+        U FNO W IN C FNO ; binary write
+        U CLOG W !,"Stripped ",CRLF," CR and LF characters, excised ",LS," string characters."
+        W !,"Input file length = ",LIN,", output file length = ",$L(IN),!,DASHLINE,!
+        ZSY "cp "_FNO_" /home/opus/share" 
         Q
 
